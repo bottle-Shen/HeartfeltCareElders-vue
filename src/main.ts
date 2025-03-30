6c7e572f56220217b1c819b6e8be92c41128939a
bottle-Shen <3198325698@qq.com>
2025-03-31 01:49:02 +0800

    first-ending

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import './styles/reset.scss'
 //Element Plus消息弹出框样式
 // import "element-plus/theme-chalk/el-loading.css";
 // import "element-plus/theme-chalk/el-message.css";
@@ -16,15 +15,6 @@ import { useDebounceDirective,useThrottleDirective } from "@/utils";
 import request from './utils/request';
 
 const app = createApp(App)
-
-//全局使用axios
-//配置请求头，非常重要，有了这个才可以正常使用POST等请求后台数据
-// Axios.defaults.headers.post['Content-Type'] = 'application/x-www-fromurlencodeed'
-// axios.defaults.xsrfCookieName = 'csrfmiddlewaretoken';
-// axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-// 跨域携带cookie
-// axios.defaults.withCredentials = true;
-// app.config.globalProperties.$axios = axios;
 
 // 挂载axios实例到Vue原型上，使其在组件中可用
 app.config.globalProperties.$request = request;
